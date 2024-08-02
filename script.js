5ca4228060820334f0a7394fee11c0998164c7d5
Courtland <courtlandpinnock@gmail.com>
2024-08-02 18:31:03 +0100

    Base file creation and basic operator functions.

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(num,num1) {
+    total = num + num1;
+    return total;   
+}
+
+function subtract(num,num1) {
+    return num - num1;
+}
+
+function multiply(num,num1) {
+return num * num1;
+}
+
+function divide(num,num1) {
+    return num / num1;
+}
+
+    
