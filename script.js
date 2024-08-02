86d321ea816e250f3da094bf70e52ffc50ecafe8
Courtland <courtlandpinnock@gmail.com>
2024-08-02 19:38:53 +0100

    Add operate function and equals btn logic
    operate function takes ARG (operate) and calls
    relevant operator function.
    
    Add equals button logic to call function operate()
    passing the current active operator store in
    variable op.

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
+var n = 10;
+var n1 = 5;
+var op = "plus";
+
+const equal = document.getElementById("equals");
+equal.addEventListener("click",(e) => {
+    operate(op);
+});
+// equal.addEventListener("click",operate(op));
+
+
 function add(num,num1) {
     total = num + num1;
+    console.log(total);
     return total;   
 }
 
 function subtract(num,num1) {
+    console.log("op minus")
     return num - num1;
 }
 
 function multiply(num,num1) {
-return num * num1;
+    return num * num1;
 }
 
 function divide(num,num1) {
     return num / num1;
 }
+
+function operate(operator) {
+    console.log(operator);
+    var total = 0;
+    if (operator === "plus") {
+        console.log("n=",n,"n1",n1)
+        return add(n,n1)
+    }
+    if (operator === "minus") {
+        return subtract(n,n1);
+    }
+    if (operator === "multiply") {
+       return multiply(n,n1);
+    }
+    if (operator === "divide") {
+        return divide(n,n1);
+    }
+}
+
 
     
